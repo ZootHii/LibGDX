e1786686c63f5304011e3a6eb47e538c55ed940f
Ahmet <ahmet-yildirim_5858@outlook.com>
2020-06-29 17:38:41 +0300

    Upgrade

--- core/src/com/zoothii/game/SecureCore.java
+++ core/src/com/zoothii/game/SecureCore.java
@@ -15,8 +15,7 @@ public class SecureCore extends ApplicationAdapter {
 	Sprite core;
 	Circle coreCircle;
 	Enemy enemy1 ,enemy2, enemy3, enemy4;
-	double enemy4Acc = 0.012; // XD
-	// deneme değişimö 
+	double enemy4Acc = 0.012;
 	double enemy1Acc = 0.068;
 	double enemy2Acc = 0.046;
 	double enemy3Acc = 0.022;
